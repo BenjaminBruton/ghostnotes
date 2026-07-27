c274fad789ed95eaee63945a12503d220a4c4b1d
BenjaminBruton <bbruton@wgu.edu>
2026-07-27 10:51:19 -0500

    changed cast name

--- app/casting/page.tsx
+++ app/casting/page.tsx
@@ -133,9 +133,9 @@ export default function Casting() {
               </div>
             </div>
 
-            {/* Derrick Keller */}
+            {/* Officer Keller */}
             <div className="bg-primary-blue bg-opacity-10 p-5 rounded-lg border border-primary-blue">
-              <h4 className="text-lg font-bold text-white mb-2">Derrick Keller</h4>
+              <h4 className="text-lg font-bold text-white mb-2">Officerk Keller</h4>
               <div className="space-y-1 text-gray-300 text-sm">
                 <p>• White male, 40s-50s</p>
                 <p>• No particular height required</p>
